9332a97500756773de4f077bdaa70146eb6210de
Steve Lukis <stevelukis88@gmail.com>
2021-07-27 18:48:40 +0800

    Added filtering functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import {nanoid} from "nanoid"
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed,
+}
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ))
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task => {
@@ -32,7 +49,9 @@ function App(props) {
     setTasks(editedTaskList);
   }
 
-  const taskList = tasks.map(task => (
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
     <Todo
       id={task.id}
       name={task.name}
@@ -61,9 +80,7 @@ function App(props) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
         {headingText}
