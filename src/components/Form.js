e6d35b09e6808937bcdf607a8f6f2f485c96a80e
Steve Lukis <stevelukis88@gmail.com>
2021-07-27 15:12:24 +0800

    Added callback prop and state to the Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Form() {
+export default function Form(props) {
+  const [name, setName] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addTask(name);
+    setName("");
+  }
+
+  function handleChange(e) {
+    setName(e.target.value)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
           What needs to be done?
@@ -14,6 +26,8 @@ export default function Form() {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
